b61d8608342fc22598f3d6c3eb54e5d87a738fc2
cpdedraj <21f3001592@student.onlinedegree.iitm.ac.in>
2022-09-03 02:12:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+require("dotenv").config();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+-app.get("/", function (req, res) {
+  +app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+});
+if (process.env.NODE_ENV === "prod") {
+  app.listen((port = 80), () => {
+    console.log(`listening at http://localhost:${port}`);
+  });
+} else {
+  app.listen((port = 8000), () => {
+    console.log(`listening at http://localhost:${port}`);
+  });
+}
